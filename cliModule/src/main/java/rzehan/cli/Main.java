cd366e2e929b35591b31581ecfe88d9e0f3e4ad9
rzeh4n <Martin.Rehanek@gmail.com>
2016-10-18 20:42:59 +0200

    validace podle xsd, priklady. zatim ne u vseho funkcni

--- cliModule/src/main/java/rzehan/cli/Main.java
+++ cliModule/src/main/java/rzehan/cli/Main.java
@@ -1,6 +1,7 @@
 package rzehan.cli;
 
 import rzehan.shared.SharedClass;
+import rzehan.shared.xsdValidation.XsdValidator;
 
 import java.io.File;
 
@@ -10,8 +11,41 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
         //String shared = "TODO";
-        String shared = new SharedClass().toString();
-        System.out.println("Hello from CLI, shared: " + shared);
+        /*String shared = new SharedClass().toString();
+        System.out.println("Hello from CLI, shared: " + shared);*/
+
+
+        //info - TODO: problem s validaci bez namespacu
+        File infoXml = new File("/home/martin/IdeaProjects/PspValidator/sharedModule/src/main/resources/rzehan/shared/fdmf_1_1_3/xsds/examples/info.xml");
+        File infoXsd = new File("/home/martin/IdeaProjects/PspValidator/sharedModule/src/main/resources/rzehan/shared/fdmf_1_1_3/xsds/alto_2.0.xsd");
+        XsdValidator.validate("INFO", infoXsd, infoXml);
+
+        //mix - ok
+        File mixXsd = new File("/home/martin/IdeaProjects/PspValidator/sharedModule/src/main/resources/rzehan/shared/fdmf_1_1_3/xsds/mix_2.0.xsd");
+        File mixXml = new File("/home/martin/IdeaProjects/PspValidator/sharedModule/src/main/resources/rzehan/shared/fdmf_1_1_3/xsds/examples/mix.xml");
+        XsdValidator.validate("MIX", mixXsd, mixXml);
+
+        //premis - ok
+        File premisXsd = new File("/home/martin/IdeaProjects/PspValidator/sharedModule/src/main/resources/rzehan/shared/fdmf_1_1_3/xsds/premis_2.2.xsd");
+        File premisXml = new File("/home/martin/IdeaProjects/PspValidator/sharedModule/src/main/resources/rzehan/shared/fdmf_1_1_3/xsds/examples/premis.xml");
+        XsdValidator.validate("PREMIS", premisXsd, premisXml);
+
+        //dc - TODO, jak je to s tim root elementem a jeste import xml.xsd v xsd
+        File dcXsd = new File("/home/martin/IdeaProjects/PspValidator/sharedModule/src/main/resources/rzehan/shared/fdmf_1_1_3/xsds/dc_1.1.xsd");
+        File dcXml = new File("/home/martin/IdeaProjects/PspValidator/sharedModule/src/main/resources/rzehan/shared/fdmf_1_1_3/xsds/examples/dc.xml");
+        XsdValidator.validate("DC", dcXsd, dcXml);
+
+        //mods - TODO: problem s importem xml.xsd
+        File modsXsd = new File("/home/martin/IdeaProjects/PspValidator/sharedModule/src/main/resources/rzehan/shared/fdmf_1_1_3/xsds/mods_3.5.xsd");
+        File modsXml = new File("/home/martin/IdeaProjects/PspValidator/sharedModule/src/main/resources/rzehan/shared/fdmf_1_1_3/xsds/examples/mods.xml");
+        XsdValidator.validate("MODS", modsXsd, modsXml);
+
+        //mets - ok
+        File metsXsd = new File("/home/martin/IdeaProjects/PspValidator/sharedModule/src/main/resources/rzehan/shared/fdmf_1_1_3/xsds/mets_1.9.1.xsd");
+        File metsXml = new File("/home/martin/IdeaProjects/PspValidator/sharedModule/src/main/resources/rzehan/shared/fdmf_1_1_3/xsds/examples/mets.xml");
+        XsdValidator.validate("METS", metsXsd, metsXml);
+
+
     }
 
 }
---
+++ sharedModule/src/main/java/rzehan/shared/xsdValidation/XsdValidator.java
@@ -0,0 +1,60 @@
+package rzehan.shared.xsdValidation;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by martin on 18.10.16.
+ */
+public class XsdValidator {
+
+    public static void validate(String label, File xsd, File xml) {
+        System.out.println("validating " + label);
+        //System.out.println("xsd: " + xsd.getAbsolutePath());
+        //System.out.println("xml: " + xml.getAbsolutePath());
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        try {
+            //Schema schema = schemaFactory.newSchema(new File(getResource(schemaFile)));
+            Schema schema = schemaFactory.newSchema(xsd);
+
+            /*SAXParserFactory spf = SAXParserFactory.newInstance();
+            spf.setValidating(false);//vypne dtd validaci
+            spf.setSchema(schema);
+
+            XMLReader xmlReader = spf.newSAXParser().getXMLReader();
+            //xmlReader.setContentHandler(...);
+            xmlReader.parse(new InputSource()); // will validate against the schema*/
+
+
+            Validator validator = schema.newValidator();
+            //validator.validate(new StreamSource(new File(getResource(xmlFile))));
+            validator.validate(new StreamSource(xml));
+            System.out.println(label + ": VALID");
+            //return true;
+        } catch (SAXException | IOException e) {
+            //e.printStackTrace();
+            System.out.println(label + ": NOT VALID");
+        } finally {
+            System.out.println();
+        }
+    }
+
+    /*private String getResource(String filename) throws FileNotFoundException {
+        URL resource = getClass().getClassLoader().getResource(filename);
+        Objects.requireNonNull(resource);
+
+        return resource.getFile();
+    }*/
+
+
+}
