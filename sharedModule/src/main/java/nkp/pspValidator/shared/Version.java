a6e031f0cae03e9792135b906c03b390bbd039c3
Martin Řehánek <Martin.Rehanek@gmail.com>
2017-01-26 00:28:26 +0100

    zvysena verze

--- sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
+++ sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
@@ -4,6 +4,6 @@ package nkp.pspValidator.shared;
  * Created by martin on 20.12.16.
  */
 public class Version {
-    public static String VERSION_CODE = "1.0";
-    public static String BUILD_DATE = "20. 12. 2016";
+    public static String VERSION_CODE = "1.1-dev";
+    public static String BUILD_DATE = "26. 1. 2017";
 }
