1e67103aee16caa22275831fcf0b507ae4378d15
Martin Řehánek <Martin.Rehanek@gmail.com>
2017-03-07 00:18:44 +0100

    aktualni verze je 1.4-dev

--- sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
+++ sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
@@ -7,6 +7,6 @@ public class Version {
     /**
      * Version code may contain up to one hyphen separating actual version code from suffix dev/alpha/beta, etc.
      */
-    public static String VERSION_CODE = "1.3";
-    public static String BUILD_DATE = "26. 2. 2017";
+    public static String VERSION_CODE = "1.4-dev";
+    public static String BUILD_DATE = "7. 3. 2017";
 }
