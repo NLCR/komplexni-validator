cd5894222088667e38bf467685c48e6487dd66ed
Martin Řehánek <Martin.Rehanek@gmail.com>
2017-07-13 16:34:04 +0200

    aktualizace verze (1.5 -> 1.6)

--- sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
+++ sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
@@ -7,6 +7,6 @@ public class Version {
     /**
      * Version code may contain up to one hyphen separating actual version code from suffix dev/alpha/beta, etc.
      */
-    public static String VERSION_CODE = "1.5";
-    public static String BUILD_DATE = "10. 4. 2017";
+    public static String VERSION_CODE = "1.6";
+    public static String BUILD_DATE = "13. 7. 2017";
 }
