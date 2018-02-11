8c2c76396c6e346737ae37b101cb30e7bb5e25cb
Martin Řehánek <Martin.Rehanek@gmail.com>
2018-02-11 17:11:12 +0100

    upravena verze

--- sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
+++ sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
@@ -7,6 +7,6 @@ public class Version {
     /**
      * Version code may contain up to one hyphen separating actual version code from suffix dev/alpha/beta, etc.
      */
-    public static String VERSION_CODE = "1.7";
+    public static String VERSION_CODE = "1.7-dev";
     public static String BUILD_DATE = "4. 2. 2018";
 }
