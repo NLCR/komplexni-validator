dde07b74ca3fb7e438b80c3b2ea55425c39e4bbe
Martin Řehánek <Martin.Rehanek@gmail.com>
2019-01-09 22:40:42 +0100

    zmena verzovani kvuli mergnuti do hlavni vetve

--- sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
+++ sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
@@ -7,6 +7,6 @@ public class Version {
     /**
      * Version code may contain up to one hyphen separating actual version code from suffix dev/alpha/beta, etc.
      */
-    public static String VERSION_CODE = "2.0.alpha-sigla_list_update";
+    public static String VERSION_CODE = "2.0.dev-01";
     public static String BUILD_DATE = "3. 1. 2018";
 }
