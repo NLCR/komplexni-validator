85d3b5937bf449c36d69f56d625fd33457ae7448
Martin Řehánek <Martin.Rehanek@gmail.com>
2019-01-15 23:24:33 +0100

    zmena verze

--- sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
+++ sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
@@ -7,6 +7,6 @@ public class Version {
     /**
      * Version code may contain up to one hyphen separating actual version code from suffix dev/alpha/beta, etc.
      */
-    public static String VERSION_CODE = "2.0-dev-uroven_txt_logu_v_gui-01";
-    public static String BUILD_DATE = "10. 1. 2018";
+    public static String VERSION_CODE = "2.0-dev-validace_zip_balicku_z_gui";
+    public static String BUILD_DATE = "15. 1. 2018";
 }
