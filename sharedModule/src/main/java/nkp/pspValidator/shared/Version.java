5e57cc9f3dc7c17c80ac615e70933b259894ae15
Martin Řehánek <Martin.Rehanek@gmail.com>
2019-01-28 10:23:52 +0100

    zmena verze

--- sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
+++ sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
@@ -7,6 +7,6 @@ public class Version {
     /**
      * Version code may contain up to one hyphen separating actual version code from suffix dev/alpha/beta, etc.
      */
-    public static String VERSION_CODE = "2.0-dev-02";
+    public static String VERSION_CODE = "2.0-dev-kontrola_urn_nbn";
     public static String BUILD_DATE = "22. 1. 2018";
 }
