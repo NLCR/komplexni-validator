5b1f8baf194d38b106d55f55677a4881ed22d079
Martin Řehánek <martin.rehanek@gmail.com>
2020-03-04 14:44:48 +0100

    Disable some testing code in nkp.pspValidator.shared.Main

--- sharedModule/src/main/java/nkp/pspValidator/shared/Main.java
+++ sharedModule/src/main/java/nkp/pspValidator/shared/Main.java
@@ -9,36 +9,34 @@ import java.security.NoSuchAlgorithmException;
 
 public class Main {
 
-    static UrnNbnResolverChecker checker = null;
-
-    private static void init() throws KeyManagementException, NoSuchAlgorithmException {
-        checker = new UrnNbnResolverChecker();
-    }
-
     public static void main(String[] args) throws IOException, KeyStoreException, KeyManagementException, NoSuchAlgorithmException, UrnNbnResolverChecker.ResolverError {
         System.out.println("hello from shared module");
-        init();
-        //ACTIVE
-        check("urn:nbn:cz:mzk-0001vw");
-        //FREE
-        check("urn:nbn:cz:mzk-xxxxxx");
-        //RESERVED
-        check("urn:nbn:cz:p01nk-000007");
-        //DEACTIVATED
-        check("urn:nbn:cz:zne001-00002o");
-
-        //wrong lang code
-        check("urn:nbn:cc:mzk-0001vw");
-        //wrong prefix
-        check("urn-nbn-cz:mzk-0001vw");
-        //invalid registrar code
-        check("urn:nbn:cz:m-0001vw");
-        //invalid document code
-        check("urn:nbn:cz:mzk-000");
-
+        //checkUrnNbns();
     }
 
-    private static void check(String urnNbn) throws IOException, KeyStoreException {
+    private static void checkUrnNbns() throws KeyManagementException, NoSuchAlgorithmException, IOException, KeyStoreException {
+        UrnNbnResolverChecker checker = new UrnNbnResolverChecker();
+        //init();
+        //ACTIVE
+        check(checker, "urn:nbn:cz:mzk-0001vw");
+        //FREE
+        check(checker, "urn:nbn:cz:mzk-xxxxxx");
+        //RESERVED
+        check(checker, "urn:nbn:cz:p01nk-000007");
+        //DEACTIVATED
+        check(checker, "urn:nbn:cz:zne001-00002o");
+
+        //wrong lang code
+        check(checker, "urn:nbn:cc:mzk-0001vw");
+        //wrong prefix
+        check(checker, "urn-nbn-cz:mzk-0001vw");
+        //invalid registrar code
+        check(checker, "urn:nbn:cz:m-0001vw");
+        //invalid document code
+        check(checker, "urn:nbn:cz:mzk-000");
+    }
+
+    private static void check(UrnNbnResolverChecker checker, String urnNbn) throws IOException, KeyStoreException {
         try {
             checker.check(urnNbn);
         } catch (UrnNbnResolverChecker.ResolverError e) {
