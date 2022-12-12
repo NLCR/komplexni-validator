f5e542c332a2b733da64bae205b1e8a92287fec0
Martin Řehánek <martin.rehanek@gmail.com>
2022-12-12 05:25:20 +0100

    update build date

--- sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
+++ sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
@@ -8,5 +8,5 @@ public class Version {
      * Version code may contain up to one hyphen separating actual version code from suffix dev/alpha/beta, etc.
      */
     public static String VERSION_CODE = "2.3";
-    public static String BUILD_DATE = "15. 12. 2021";
+    public static String BUILD_DATE = "12. 12. 2022";
 }
