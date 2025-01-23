554eac464e273688e5dbcc6982a21730e0e1a395
Jan Bilwachs <jan.bilwachs@gmail.com>
2025-01-23 14:20:00 +0100

    fix release date

--- modules/sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
+++ modules/sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
@@ -8,5 +8,5 @@ public class Version {
      * Version code may contain up to one hyphen separating actual version code from suffix dev/alpha/beta, etc.
      */
     public static String VERSION_CODE = "2.5-beta";
-    public static String BUILD_DATE = "23. 1. 2024";
+    public static String BUILD_DATE = "23. 1. 2025";
 }
