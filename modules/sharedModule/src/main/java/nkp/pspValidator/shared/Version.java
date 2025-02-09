124c10f516d6ee685f651b0318f613cb035e61ab
Jan Bilwachs <jan.bilwachs@gmail.com>
2025-02-09 22:12:36 +0100

    change version 2.5-beta to 2.5

--- modules/sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
+++ modules/sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
@@ -7,6 +7,6 @@ public class Version {
     /**
      * Version code may contain up to one hyphen separating actual version code from suffix dev/alpha/beta, etc.
      */
-    public static String VERSION_CODE = "2.5-beta";
+    public static String VERSION_CODE = "2.5";
     public static String BUILD_DATE = "23. 1. 2025";
 }
