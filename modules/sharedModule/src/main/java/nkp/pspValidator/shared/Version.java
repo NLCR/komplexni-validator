a8bcff161deea1c578d22686418758b097ace7aa
Jan Bilwachs <jan.bilwachs@gmail.com>
2025-11-07 14:30:56 +0100

    set version to 2.6.1

--- modules/sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
+++ modules/sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
@@ -7,6 +7,6 @@ public class Version {
     /**
      * Version code may contain up to one hyphen separating actual version code from suffix dev/alpha/beta, etc.
      */
-    public static String VERSION_CODE = "2.6.1-beta";
-    public static String BUILD_DATE = "10. 10. 2025";
+    public static String VERSION_CODE = "2.6.1";
+    public static String BUILD_DATE = "7. 11. 2025";
 }
