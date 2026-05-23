4ea7aac4216d76afb06521fc33c1b14b190d005b
Jan Bilwachs <jan.bilwachs@gmail.com>
2026-05-23 14:01:06 +0200

    issue #224: checkIdentifiersNoDuplicates: WARNING -> ERROR

--- modules/sharedModule/src/main/java/nkp/pspValidator/shared/engine/validationFunctions/VfCheckIdentifiersNoDuplicateTypes.java
+++ modules/sharedModule/src/main/java/nkp/pspValidator/shared/engine/validationFunctions/VfCheckIdentifiersNoDuplicateTypes.java
@@ -76,7 +76,7 @@ public class VfCheckIdentifiersNoDuplicateTypes extends ValidationFunction {
                     map.put(id.getType(), id.getValue());
                 } else {
                     String presentValue = map.get(id.getType());
-                    result.addError(new ValidationProblem(Level.WARNING, String.format("seznam obsahuje více identifikátorů typu '%s', např: '%s', '%s'", id.getType(), id, new Identifier(id.getType(), presentValue)))
+                    result.addError(new ValidationProblem(Level.ERROR, String.format("seznam obsahuje více identifikátorů typu '%s', např: '%s', '%s'", id.getType(), id, new Identifier(id.getType(), presentValue)))
                             .withSimpleMessage("seznam obsahuje více identifikátorů stejného typu")
                             .withLabel("id_type=" + id.getType())
                             .withReferencedValue(String.format("'%s','%s'", id, new Identifier(id.getType(), presentValue)))
--- modules/sharedModule/src/main/java/nkp/pspValidator/shared/engine/validationFunctions/VfCheckIdentifiersNoDuplicates.java
+++ modules/sharedModule/src/main/java/nkp/pspValidator/shared/engine/validationFunctions/VfCheckIdentifiersNoDuplicates.java
@@ -89,7 +89,7 @@ public class VfCheckIdentifiersNoDuplicates extends ValidationFunction {
                     if (valueFound == null) {
                         map.put(id.getType(), id.getValue());
                     } else if (valueFound.equals(id.getValue())) {
-                        result.addError(new ValidationProblem(Level.WARNING, String.format("seznam obsahuje duplikovaný identifikátor '%s'", id.toString()))
+                        result.addError(new ValidationProblem(Level.ERROR, String.format("seznam obsahuje duplikovaný identifikátor '%s'", id.toString()))
                                 .withSimpleMessage("seznam obsahuje duplikovaný identifikátor")
                                 .withLabel("id_type=" + id.getType())
                                 .withReferencedValue(id.toString())
