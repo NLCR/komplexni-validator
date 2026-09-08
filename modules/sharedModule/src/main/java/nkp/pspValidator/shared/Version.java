82e5900b8061a20542977e54b4ad61c348c16b05
Martin Řehánek <martin.rehanek@trinera.cz>
2026-09-08 10:59:17 +0200

    set version to 2.8-beta

--- modules/sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
+++ modules/sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
@@ -7,6 +7,6 @@ public class Version {
     /**
      * Version code may contain up to one hyphen separating actual version code from suffix dev/alpha/beta, etc.
      */
-    public static String VERSION_CODE = "2.7-dev"; //cast pred pomlckou musi odpovidat validatorVersion v konfiguracnich souborech (fDMF, profily)
-    public static String BUILD_DATE = "7. 9. 2026";
+    public static String VERSION_CODE = "2.8-beta"; //cast pred pomlckou musi odpovidat validatorVersion v konfiguracnich souborech (fDMF, profily)
+    public static String BUILD_DATE = "8. 9. 2026";
 }
