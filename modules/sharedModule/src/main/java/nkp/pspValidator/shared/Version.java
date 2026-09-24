cde62bc936956809aaa26b1bb5c6afec71e07b0f
Martin Řehánek <martin.rehanek@trinera.cz>
2026-09-24 11:16:28 +0200

    set build date for 2.8-beta, fix jpackage app version
    jpackage rejected "2.8." derived from "2.8-beta"; trim leading/trailing dots.

--- modules/sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
+++ modules/sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
@@ -8,5 +8,5 @@ public class Version {
      * Version code may contain up to one hyphen separating actual version code from suffix dev/alpha/beta, etc.
      */
     public static String VERSION_CODE = "2.8-beta"; //cast pred pomlckou musi odpovidat validatorVersion v konfiguracnich souborech (fDMF, profily)
-    public static String BUILD_DATE = "8. 9. 2026";
+    public static String BUILD_DATE = "24. 9. 2026";
 }
